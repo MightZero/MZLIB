b70911ffc967f4e1ae551b5992f24c95a7a8c1da
MZR <1904769228@qq.com>
2024-12-23 20:43:00 +0800

    refactor(matrix): 重构矩阵类实现
    - 移除了 Static_Matrix 类
    - 重新设计了 Matrix 类的内存布局和操作
    - 添加了对 std::vector 的支持，提高了内存使用效率
    - 优化了矩阵乘法和加法的实现
    - 改进了异常处理和边界检查

--- matrix.hpp
+++ matrix.hpp
@@ -1,10 +1,9 @@
-// FileName : matrix.hpp
-// Matrix Packed Class Header
-// Programmed By MightZero
-// Copyright 2024 MightZero
 #include <utility>
 #include <initializer_list>
+#include <vector>
 #include <stdexcept>
+#include <iterator>
+
 #pragma once
 #ifndef _MZLIB_MATRIX_HPP
 #define _MZLIB_MATRIX_HPP
@@ -12,299 +11,116 @@
 
 namespace MZLIB
 {
-    template <typename _T>
-    class Matrix;
-
-    template <size_t _H, size_t _W, typename _T>
-    class Static_Matrix;
-
     template <typename _T = int>
     class Matrix
     {
     public:
         using element_type = _T;
         using reference = element_type &;
+        using const_reference = const element_type &;
         using pointer = element_type *;
-        using dpointer = pointer *;
-        Matrix(size_t H, size_t W) : _H(H), _W(W) { allocate(); }
-        ~Matrix() { free(); }
-        Matrix(const Matrix &other) : _H(other._H), _W(other._W)
+        using const_pointer = const element_type *;
+
+        Matrix() : _H(0), _W(0), _dat() {}
+        Matrix(size_t H, size_t W, const element_type &x = element_type()) noexcept : _H(H), _W(W)
         {
-            allocate();
-            for (size_t i = 0; i < _H; ++i)
-                std::copy(other.mat[i], other.mat[i] + _W, mat[i]);
-        }
-        Matrix(Matrix &&other) noexcept : mat(other.mat), _H(other._H), _W(other._W)
-        {
-            other.mat = nullptr, other._H = 0, other._W = 0;
+            _dat.assign(this->size(), x);
         }
         Matrix(std::initializer_list<std::initializer_list<element_type>> _ls)
         {
+            if (_ls.size() == 0)
+            {
+                throw std::length_error("invalid initializer list size");
+            }
             _H = _ls.size(), _W = (*_ls.begin()).size();
-            allocate();
             size_t _nH = 0, _nW = 0;
             for (auto &_lsh : _ls)
             {
                 if (_lsh.size() != _W)
-                {
-                    free();
                     throw std::length_error("invalid initializer list size");
-                }
                 for (auto &x : _lsh)
-                {
-                    mat[_nH][_nW] = x;
-                    ++_nW == _W ? _nW = 0, ++_nH : 0;
-                }
+                    getVal(_nH, _nW) = x,
+                                ++_nW == _W ? _nW = 0, ++_nH : 0;
             }
         }
-        inline Matrix &operator=(const Matrix &other)
+        inline reference getVal(size_t x, size_t y)
         {
-            if (this != &other)
-            {
-                free();
-                _H = other._H, _W = other._W;
-                allocate();
-                for (size_t i = 0; i < _H; ++i)
-                    std::copy(other.mat[i], other.mat[i] + _W, mat[i]);
-            }
-            return *this;
-        }
-
-        inline Matrix &operator=(Matrix &&other) noexcept
-        {
-            if (this != &other)
-            {
-                free();
-                mat = other.mat, _H = other._H, _W = other._W;
-                other.mat = nullptr, other._H = 0, other._W = 0;
-            }
-            return *this;
-        }
-        inline operator dpointer() noexcept { return mat; }
-        inline reference getVal(const size_t &x, const size_t &y)
-        {
-            if (x > _H || y > _W)
+            if (x >= _H || y >= _W)
                 throw std::out_of_range("invalid access");
-            return mat[x][y];
+            return _dat[x * _W + y];
         }
-        inline const reference getVal(const size_t &x, const size_t &y) const
+        inline const_reference getVal(size_t x, size_t y) const
         {
-            if (x > _H || y > _W)
+            if (x >= _H || y >= _W)
                 throw std::out_of_range("invalid access");
-            return mat[x][y];
+            return _dat[x * _W + y];
         }
-        inline constexpr size_t getH() const { return _H; }
-        inline constexpr size_t getW() const { return _W; }
-        inline constexpr size_t size() const { return _H * _W; }
-        class iterator
+        inline typename std::vector<element_type>::iterator operator[](size_t x)
         {
-            Matrix &_M;
-            size_t _nH, _nW;
+            return _dat.begin() + x * _W;
+        }
+        inline typename std::vector<element_type>::const_iterator operator[](size_t x) const
+        {
+            return _dat.begin() + x * _W;
+        }
+        inline constexpr size_t getH() const noexcept { return _H; }
+        inline constexpr size_t getW() const noexcept { return _W; }
+        inline constexpr size_t size() const noexcept { return _H * _W; }
 
-        public:
-            iterator(Matrix &M, size_t nH, size_t nW) : _M(M), _nH(nH), _nW(nW) {}
-            inline reference operator*() { return _M.getVal(_nH, _nW); }
-            inline iterator &operator++()
-            {
-                ++_nW == _M.getW() ? _nW = 0, ++_nH : 0;
-                return *this;
-            }
-            inline bool operator!=(const iterator &x) noexcept { return !(_nH == x._nH && _nW == x._nW); }
-            inline pointer operator->() { return &_M.getVal(_nH, _nW); }
-        };
-        inline iterator begin() noexcept { return iterator(*this, 0, 0); }
-        inline iterator end() noexcept { return iterator(*this, _H, 0); }
+        using iterator = typename std::vector<element_type>::iterator;
+        using const_iterator = typename std::vector<element_type>::const_iterator;
+        using reverse_iterator = std::reverse_iterator<iterator>;
+        using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+        inline iterator begin() noexcept { return _dat.begin(); }
+        inline iterator end() noexcept { return _dat.end(); }
+        inline const_iterator begin() const noexcept { return _dat.cbegin(); }
+        inline const_iterator end() const noexcept { return _dat.cend(); }
+        inline const_iterator cbegin() const noexcept { return _dat.cbegin(); }
+        inline const_iterator cend() const noexcept { return _dat.cend(); }
+        inline reverse_iterator rbegin() noexcept { return _dat.rbegin(); }
+        inline reverse_iterator rend() noexcept { return _dat.rend(); }
+        inline const_reverse_iterator rbegin() const noexcept { return _dat.crbegin(); }
+        inline const_reverse_iterator rend() const noexcept { return _dat.crend(); }
+        inline const_reverse_iterator crbegin() const noexcept { return _dat.crbegin(); }
+        inline const_reverse_iterator crend() const noexcept { return _dat.crend(); }
 
     private:
         size_t _H, _W;
-        dpointer mat;
-        inline void free()
-        {
-            if (mat == nullptr)
-                return;
-            for (size_t i = 0; i < _H; ++i)
-                if (mat[i])
-                    delete[] mat[i];
-            delete[] mat;
-            mat = nullptr;
-        }
-        inline void allocate()
-        {
-            mat = new pointer[_H];
-            for (size_t i = 0; i < _H; ++i)
-                mat[i] = new element_type[_W]{};
-        }
+        std::vector<element_type> _dat;
     };
+
     template <typename _T>
     inline Matrix<_T> operator*(const Matrix<_T> &x, const Matrix<_T> &y)
     {
+        if (!x.size())
+            return x;
+        if (!y.size())
+            return y;
         if (x.getW() != y.getH())
             throw std::invalid_argument("invalid matrix size");
         size_t _Hx = x.getH(), _Wy = y.getW(), _HW = x.getW();
-        Matrix<_T> res = Matrix<_T>(_Hx, _Wy);
-        for (auto i = 0; i < _Hx; ++i)
-            for (auto j = 0; j < _Wy; ++j)
-                for (auto k = 0; k < _HW; ++k)
+        Matrix<_T> res(_Hx, _Wy);
+        for (size_t i = 0; i < _Hx; ++i)
+            for (size_t j = 0; j < _Wy; ++j)
+                for (size_t k = 0; k < _HW; ++k)
                     res.getVal(i, j) = res.getVal(i, j) + x.getVal(i, k) * y.getVal(k, j);
         return res;
     }
+
     template <typename _T>
     inline Matrix<_T> operator+(const Matrix<_T> &x, const Matrix<_T> &y)
     {
+        if (!x.size())
+            return y;
+        if (!y.size())
+            return x;
         if (x.getH() != y.getH() || x.getW() != y.getW())
             throw std::invalid_argument("invalid matrix size");
         size_t _H = x.getH(), _W = x.getW();
-        Matrix<_T> res = Matrix<_T>(_H, _W);
-        for (auto i = 0; i < _H; ++i)
-            for (auto j = 0; j < _W; ++j)
-                res.getVal(i, j) = x.getVal(i, j) + y.getVal(i, j);
-        return res;
-    }
-
-    template <size_t _H, size_t _W, typename _T = int>
-    class Static_Matrix
-    {
-    public:
-        using element_type = _T;
-        using reference = element_type &;
-        using pointer = element_type *;
-        using dpointer = pointer *;
-        Static_Matrix() { allocate(); }
-        ~Static_Matrix() { free(); }
-        Static_Matrix(const Static_Matrix &other)
-        {
-            allocate();
-            for (size_t i = 0; i < _H; ++i)
-                std::copy(other.mat[i], other.mat[i] + _W, mat[i]);
-        }
-        Static_Matrix(Static_Matrix &&other) noexcept : mat(other.mat)
-        {
-            other.mat = nullptr;
-        }
-        Static_Matrix(std::initializer_list<element_type> _ls)
-        {
-            if (_ls.size() != size())
-                throw std::length_error("invalid initializer list size");
-            allocate();
-            size_t _nH = 0, _nW = 0;
-            for (auto &x : _ls)
-            {
-                mat[_nH][_nW] = x;
-                ++_nW == _W ? _nW = 0, ++_nH : 0;
-            }
-        }
-        Static_Matrix(std::initializer_list<std::initializer_list<element_type>> _ls)
-        {
-            if (_ls.size() != _H)
-                throw std::length_error("invalid initializer list size");
-            allocate();
-            size_t _nH = 0, _nW = 0;
-            for (auto &_lsh : _ls)
-            {
-                if (_lsh.size() != _W)
-                {
-                    free();
-                    throw std::length_error("invalid initializer list size");
-                }
-                for (auto &x : _lsh)
-                {
-                    mat[_nH][_nW] = x;
-                    ++_nW == _W ? _nW = 0, ++_nH : 0;
-                }
-            }
-        }
-        inline Static_Matrix &operator=(const Static_Matrix &other)
-        {
-            if (this != &other)
-            {
-                free();
-                allocate();
-                for (size_t i = 0; i < _H; ++i)
-                    std::copy(other.mat[i], other.mat[i] + _W, mat[i]);
-            }
-            return *this;
-        }
-
-        inline Static_Matrix &operator=(Static_Matrix &&other) noexcept
-        {
-            if (this != &other)
-            {
-                free();
-                mat = other.mat;
-                other.mat = nullptr;
-            }
-            return *this;
-        }
-        inline operator dpointer() noexcept { return mat; }
-        inline reference getVal(const size_t &x, const size_t &y)
-        {
-            if (x > _H || y > _W)
-                throw std::out_of_range("invalid access");
-            return mat[x][y];
-        }
-        inline const reference getVal(const size_t &x, const size_t &y) const
-        {
-            if (x > _H || y > _W)
-                throw std::out_of_range("invalid access");
-            return mat[x][y];
-        }
-        inline constexpr size_t getH() const { return _H; }
-        inline constexpr size_t getW() const { return _W; }
-        inline constexpr size_t size() const { return _H * _W; }
-        class iterator
-        {
-            Static_Matrix &_M;
-            size_t _nH, _nW;
-
-        public:
-            iterator(Static_Matrix &M, size_t nH, size_t nW) : _M(M), _nH(nH), _nW(nW) {}
-            inline reference operator*() { return _M.getVal(_nH, _nW); }
-            inline iterator &operator++()
-            {
-                ++_nW == _M.getW() ? _nW = 0, ++_nH : 0;
-                return *this;
-            }
-            inline bool operator!=(const iterator &x) noexcept { return !(_nH == x._nH && _nW == x._nW); }
-            inline pointer operator->() { return &_M.getVal(_nH, _nW); }
-        };
-        inline iterator begin() noexcept { return iterator(*this, 0, 0); }
-        inline iterator end() noexcept { return iterator(*this, _H, 0); }
-
-    private:
-        dpointer mat;
-        inline void free()
-        {
-            if (mat == nullptr)
-                return;
-            for (size_t i = 0; i < _H; ++i)
-                if (mat[i])
-                    delete[] mat[i];
-            delete[] mat;
-            mat = nullptr;
-        }
-        inline void allocate()
-        {
-            mat = new pointer[_H];
-            for (size_t i = 0; i < _H; ++i)
-                mat[i] = new element_type[_W]{};
-        }
-    };
-    template <size_t _Hx, size_t _HW, size_t _Wy, typename _T>
-    inline Static_Matrix<_Hx, _Wy, _T> operator*(const Static_Matrix<_Hx, _HW, _T> &x, const Static_Matrix<_HW, _Wy, _T> &y)
-    {
-        Static_Matrix<_Hx, _Wy, _T> res = Static_Matrix<_Hx, _Wy, _T>();
-        for (auto i = 0; i < _Hx; ++i)
-            for (auto j = 0; j < _Wy; ++j)
-                for (auto k = 0; k < _HW; ++k)
-                    res.getVal(i, j) = res.getVal(i, j) + x.getVal(i, k) * y.getVal(k, j);
-        return res;
-    }
-    template <size_t _H, size_t _W, typename _T>
-    inline Static_Matrix<_H, _W, _T> operator+(const Static_Matrix<_H, _W, _T> &x, const Static_Matrix<_H, _W, _T> &y)
-    {
-        Static_Matrix<_H, _W, _T> res = Static_Matrix<_H, _W, _T>();
-        for (auto i = 0; i < _H; ++i)
-            for (auto j = 0; j < _W; ++j)
+        Matrix<_T> res(_H, _W);
+        for (size_t i = 0; i < _H; ++i)
+            for (size_t j = 0; j < _W; ++j)
                 res.getVal(i, j) = x.getVal(i, j) + y.getVal(i, j);
         return res;
     }
